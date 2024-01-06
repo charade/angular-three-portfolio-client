6bac69e3623ffbc514a9de387ff77ec5c8e4d7ae
charade <charles.ekomie@gmail.com>
2024-01-06 01:29:19 +0100

    add shadow to thinker in progress...

--- src/app/routes/main/components/three/thinker.component.ts
+++ src/app/routes/main/components/three/thinker.component.ts
@@ -6,22 +6,32 @@ import {
   OnDestroy,
   Output,
   ViewContainerRef,
-  WritableSignal,
   inject,
-  signal,
 } from '@angular/core';
 import gsap from 'gsap';
 import { Subscription, distinctUntilChanged, skip } from 'rxjs';
 import { THREE_ENUMS } from 'src/app/common-utils/enums/three.enum';
 import {
+  AmbientLight,
   BasicShadowMap,
+  DirectionalLight,
+  DirectionalLightHelper,
   HemisphereLight,
+  Mesh,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
+  PCFSoftShadowMap,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
   SpotLight,
+  Vector2,
   WebGLRenderer,
 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 
 @Component({
   standalone: true,
@@ -43,19 +53,41 @@ export class ThinkerModelComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.#viewContainerRef.appendChild(this.#renderer.domElement);
-
     this.#scene.position.set(0.35, -1, 0);
+    this.#renderer.shadowMap.enabled = true;
+    this.#renderer.shadowMap.type = PCFSoftShadowMap;
 
     this.#mountCamera();
     this.#mountLights();
+    this.#addGround();
     this.#loadThinkerModel();
+
+    const effectComposer = new EffectComposer(this.#renderer);
+    const renderScene = new RenderPass(this.#scene, this.#camera);
+    const bloom = new UnrealBloomPass(
+      new Vector2(window.innerWidth, window.innerHeight),
+      1.6,
+      0.1,
+      0.1
+    );
+    effectComposer.addPass(renderScene);
+    effectComposer.addPass(bloom);
+
+    this.#configRenderer(effectComposer);
+
+    window.addEventListener('resize', () =>
+      this.#onResizeWindow(effectComposer)
+    );
+    window.addEventListener('mousemove', (event) => {
+      gsap.to(this.#scene.rotation, { y: event.clientX / 1000 });
+    });
 
     this.#subscription = this.onLoadThinkerComplete
       .pipe(skip(1), distinctUntilChanged())
       .subscribe(() =>
         this.animationTimeLine
           .to(this.#camera.position, {
-            z: 2,
+            z: 3,
             delay: 0.2,
             duration: 1,
           })
@@ -68,12 +100,6 @@ export class ThinkerModelComponent implements AfterViewInit, OnDestroy {
             },
           })
       );
-
-    this.#configRenderer();
-    window.addEventListener('resize', () => this.#onResizeWindow());
-    window.addEventListener('mousemove', (event) => {
-      gsap.to(this.#scene.rotation, { y: event.clientX / 1000 });
-    });
   }
 
   ngOnDestroy(): void {
@@ -91,46 +117,67 @@ export class ThinkerModelComponent implements AfterViewInit, OnDestroy {
     this.#camera.position.set(0, 0, -5);
   }
 
-  #onResizeWindow() {
+  #onResizeWindow(effectComposer: EffectComposer) {
     this.#camera.aspect =
       this.#viewContainerRef.offsetWidth / this.#viewContainerRef.offsetHeight;
     this.#camera.updateProjectionMatrix();
-    this.#configRenderer();
+    this.#configRenderer(effectComposer);
+  }
+
+  #addGround() {
+    const ground = new Mesh(
+      new PlaneGeometry(5, 5, 10, 10),
+      new MeshStandardMaterial({ color: 'rgb(254, 249, 245)' })
+    );
+    ground.rotation.x = 80;
+    ground.castShadow = false;
+    ground.receiveShadow = true;
+
+    ground.position.set(0.3, -0.1, -1);
+    this.#scene.add(ground);
   }
 
   #mountLights() {
-    const hemisphereLight = new HemisphereLight();
-    const frontDownPointLight = new SpotLight(0xffffff);
-    const frontUpPointLight = new SpotLight(0xffff0);
-    frontDownPointLight.castShadow = true;
-    frontUpPointLight.castShadow = true;
+    const ambientLight = new AmbientLight(0x101010);
+    // const frontDownPointLight = new SpotLight(0xffffff);
+    // const frontUpPointLight = new SpotLight(0xffff0);
+    // frontDownPointLight.castShadow = true;
+    // frontUpPointLight.castShadow = true;
+    const light = new DirectionalLight(0xffffff);
+    light.castShadow = true;
 
-    frontDownPointLight.position.set(0, 0, 2);
-    frontUpPointLight.position.set(-0.5, 1, 2);
-    this.#scene.add(frontDownPointLight);
-    this.#scene.add(frontUpPointLight);
-    this.#scene.add(hemisphereLight);
+    light.position.set(-1, 2, 1); //default; light shining from top
+    // frontDownPointLight.position.set(0, 0, 2);
+    // frontUpPointLight.position.set(-0.5, 1, 2);
+
+    light.shadow.mapSize = new Vector2(512, 512);
+    light.shadow.camera.near = 0;
+    light.shadow.camera.far = 100;
+    // this.#scene.add(frontDownPointLight);
+    // this.#scene.add(frontUpPointLight);
+    this.#scene.add(ambientLight);
+    this.#scene.add(light);
+    this.#scene.add(new DirectionalLightHelper(light, 3, 0xfff));
   }
 
-  #configRenderer() {
+  #configRenderer(effectComposer: EffectComposer) {
     this.#renderer.setSize(window.innerWidth, window.innerHeight);
     this.#renderer.setPixelRatio(devicePixelRatio);
-    this.#renderer.shadowMap.enabled = true;
-    this.#renderer.shadowMap.type = BasicShadowMap;
-    this.#renderer.render(this.#scene, this.#camera);
+    effectComposer.render();
   }
 
   #loadThinkerModel() {
     new GLTFLoader().load(
       THREE_ENUMS.MODELS.THINKER,
       ({ scene }) => {
+        this.#scene.add(scene);
+
         scene.traverse(function (node) {
           if (node.type === 'Mesh') {
             node.castShadow = true;
-            node.receiveShadow = true;
+            // node.receiveShadow = true;
           }
         });
-        this.#scene.add(scene);
 
         this.#renderer.setClearColor('rgb(254, 249, 245)');
         this.#renderer.setAnimationLoop(() => {
